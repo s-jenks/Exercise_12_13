fa0bd71e6f7340a53d782cc43d465aa78cc08766
Salma <salmaibrahim813@gmail.com>
2023-02-13 21:02:20 +0000

    corrected typos

--- exercise_13/write_file.py
+++ exercise_13/write_file.py
@@ -1,14 +1,15 @@
-f = open("pelican.txt","w")
-f= open("pelican.txt","a")
+f = open("pelican.txt", "w")
+f = open("pelican.txt", "a")
 f.write("A wonderful bird is the pelican\n")
 f.write("His bill holds more than his belican")
 f.close()
-
-f = open("pelican.txt","r")
+# open the file to read
+f = open("pelican.txt", "r")
 print(f.read())
-
+# open the file and appending which allows you to make changes to a file and adds to the end
 f = open("pelican.txt", "a")
-f.writelines(["He can take in his break,\n","Enough food for a week,\n","But I am damned if I see how the helican.\n"])
-f= open("pelican.txt","r")
+# \n represents a new line
+f.writelines(["He can take in his beak,\n", "Enough food for a week,\n", "But I'm damned if I see how the helican.\n"])
+f = open("pelican.txt", "r")
+# read allows the text to be outputted in the console
 print(f.read())
-
